110d2d4276d6f0f01634131a36829a377af831df
Robert Pedersen <128043312+Robes13@users.noreply.github.com>
2023-08-24 20:15:49 +0200

    randomNumber.sql

--- randomNumber.sql
+++ randomNumber.sql
@@ -1,34 +1,34 @@
--- Making sure I am using my Performance database
+-- Making sure I am using my Performance database --
 USE Performance;
--- Testing if dbo.LookNumber procedure exists, and if it does we drop it.
+-- Testing if dbo.LookNumber procedure exists, and if it does we drop it. --
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.LookNumber'))
     DROP PROCEDURE dbo.LookNumber;
 GO
--- Creating dbo.LookNumber
+-- Creating dbo.LookNumber --
 CREATE PROCEDURE dbo.LookNumber
-AS-- We are searching how many times the number 4711 is in our database here.
+AS-- We are searching how many times the number 4711 is in our database here. --
 	SELECT * FROM dbo.Random WHERE Random.RandomNumber = 4711;
 GO
--- Same thing here but with dbo.Frequency.
+-- Same thing here but with dbo.Frequency. --
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.Frequency'))
     DROP PROCEDURE dbo.Frequency;
 GO
 
 CREATE PROCEDURE dbo.Frequency
-AS-- Here we get all the numbers in the list, but sorted and grouped in the desired way.
+AS-- Here we get all the numbers in the list, but sorted and grouped in the desired way. --
 	SELECT RandomNumber, COUNT(RandomNumber) AS Frequency FROM Random
 	GROUP BY RandomNumber
 	ORDER BY COUNT(RandomNumber) DESC;
 GO
--- Here I am testing if our RandomNumber is an index, and if it is we drop it.
+-- Here I am testing if our RandomNumber is an index, and if it is we drop it. --
 IF EXISTS (SELECT * FROM sys.indexes WHERE name = ('index_number'))
 	DROP INDEX index_number ON Random;
 GO
--- Here we are creating said index.
+-- Here we are creating said index. --
 CREATE INDEX index_number
 	ON Random (RandomNumber);
 GO
--- Running the procedures.
+-- Running the procedures. --
 dbo.LookNumber;
 GO
 dbo.Frequency;
